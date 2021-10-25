75efa3a50d548ea31d75e14e25586a72d8a1a1ea
Kelvin Wieth <wiethkelvin@gmail.com>
2021-10-25 11:19:40 -0300

    Add quiz page

---
+++ lib/quiz/presentation/pages/quiz_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuizPage extends StatelessWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quiz'),
+      ),
+      body: const Center(
+        child: Text('Quiz!'),
+      ),
+    );
+  }
+}
