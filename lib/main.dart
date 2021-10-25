74e90733300f704e6a02ba9c20bfc4e72fb92b1c
Kelvin Wieth <wiethkelvin@gmail.com>
2021-10-25 13:33:04 -0300

    Add http certificate

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,39 @@
-import 'package:bmw_quiz_flutter/quiz/presentation/pages/quiz/quiz_page.dart';
+import 'dart:io';
+
+import 'package:bmw_quiz_flutter/quiz/infra/repositories/quiz_repository.dart';
+import 'package:bmw_quiz_flutter/quiz/presentation/cubit/quiz_bloc.dart';
+import 'package:bmw_quiz_flutter/quiz/presentation/pages/quiz_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const BMWQuizApp());
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(BMWQuizApp());
 }
 
 class BMWQuizApp extends StatelessWidget {
-  const BMWQuizApp({Key? key}) : super(key: key);
+  final QuizRepository repository;
+  BMWQuizApp({Key? key})
+      : repository = QuizRepository(),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: QuizPage(),
+    return BlocProvider(
+      create: (context) => QuizCubit(repository: repository),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: QuizPage(),
+      ),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
