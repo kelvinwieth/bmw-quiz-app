684da91e42c6de8117f36840559e87a833a5ba6f
Kelvin Wieth <wiethkelvin@gmail.com>
2021-10-26 09:48:35 -0300

    Add http timeout

--- lib/quiz/infra/datasources/quiz_api.dart
+++ lib/quiz/infra/datasources/quiz_api.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:bmw_quiz_flutter/quiz/infra/datasources/dtos/question_response.dart';
 import 'package:bmw_quiz_flutter/quiz/infra/datasources/dtos/result_request.dart';
 import 'package:bmw_quiz_flutter/quiz/infra/datasources/dtos/result_response.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class QuizApi {
@@ -11,7 +9,7 @@ class QuizApi {
 
   Future<List<QuestionResponse>> getQuestionsAsync() async {
     var url = Uri.parse(baseUrl);
-    var response = await http.get(url);
+    var response = await http.get(url).timeout(const Duration(seconds: 30));
 
     var decodedBody = jsonDecode(response.body) as List;
 
@@ -24,12 +22,9 @@ class QuizApi {
       url,
       body: request.toJson(),
       headers: {"Content-Type": "application/json"},
-    );
+    ).timeout(const Duration(seconds: 30));
 
     var json = jsonDecode(response.body);
-
-    debugPrint(json.toString());
-
     return ResultResponse.fromMap(json);
   }
 }
